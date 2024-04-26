6b8d36d7092e1515a2cdf78c1a6f3fb9fa7f87d1
Stuart Green [Check Point] <71756633+chkp-stuartgreen@users.noreply.github.com>
2024-04-26 14:46:54 +0100

    limited policy - basic example on unprotected branch

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "checkpoint" {
+  server   = var.smart1_server
+  api_key = var.smart1_api_key
+  context  = "web_api"
+  cloud_mgmt_id = var.smart1_cloud_id
+  session_name = "Terraform Session"
+  auto_publish_batch_size = 1
+}
+
+resource "checkpoint_management_access_rule" "rule1-subnet_a_to_b_icmp" {
+  name = "SubnetA-B ping"
+  layer = "LimitedScopeLayer1"
+  #layer = "Network"
+  position = {top = "top"}
+  source = ["Any"] 
+  service = ["icmp-requests"]
+  destination = ["Any"]
+  track = {
+    type = "Log"
+  }
+  action = "Accept"
+}
